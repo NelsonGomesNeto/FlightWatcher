c497ed6fbb8b46f04801a7510b206eb13e4acaf0
NelsonGomesNeto <ngn@ic.ufal.br>
2018-04-11 18:53:11 -0300

    YaY, getting flights from Latam, now I've got to create a bot to keep searching each 15 mimutes

--- sendEmail.py
+++ sendEmail.py
@@ -1,14 +1,33 @@
-import smtplib
-fromMail = "nelsongomesneto15@gmail.com"
+import json
+import requests
+import urllib3
+from lxml import html
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+api = "https://script.google.com/macros/s/AKfycbx-yKneSjj1gS_vbiGEv-mDKOd5eywt2dcIYIYSGNvacjec5dau/exec"
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.connect('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-server.ehlo()
+def sendEmail(link):
+    data = {"link": link}
+    r = requests.post(api, json.dumps(data))
+    if (r.status_code == 200):
+        print("Successfully sent e-mail! :)")
+    else:
+        print(r.status_code, r.reason)
 
-server.login(fromMail, PASSWORDEXPOSED)
+def mountString(best):
+    return("Flight " + best[1] + ": R$" + str(best[0]))
 
-message = "\nHello!"
+def searchFromLatam():
+    latamAPI = "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?country=BR&language=PT&home=pt_br&origin=MCZ&destination=CWB&departure=2018-05-23&adult=1&cabin=Y"
+    latam = requests.get(latamAPI)
+    latam = json.loads(latam.content.decode())
+    best = [99999, 0]
+    for flight in latam["data"]["flights"]:
+        print("Flight: ", flight["flightCode"], " R$", flight["cabins"][0]["displayPrice"], sep='')
+        if (flight["cabins"][0]["displayPrice"] < best[0]):
+            best = [flight["cabins"][0]["displayPrice"], flight["flightCode"]]
+    print("Best Flight Found:", best)
+    sendEmail("Latam %s" % mountString(best))
 
-server.sendmail(fromMail, "ngomesneto@outlook.com", message)
+searchFromLatam()
+
