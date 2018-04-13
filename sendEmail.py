637fd2e6a5e22c06514e1e7217ab63fcc86f3bb3
João Correia <jlmc@ic.ufl.br>
2018-04-13 11:28:57 -0300

    Nice, it's working flawlessly

--- sendEmail.py
+++ sendEmail.py
@@ -1,4 +1,5 @@
 import json
+import time
 import requests
 import urllib3
 from lxml import html
@@ -6,28 +7,42 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 api = "https://script.google.com/macros/s/AKfycbx-yKneSjj1gS_vbiGEv-mDKOd5eywt2dcIYIYSGNvacjec5dau/exec"
 
-def sendEmail(link):
-    data = {"link": link}
+def sendEmail(message):
+    data = {"link": message}
     r = requests.post(api, json.dumps(data))
     if (r.status_code == 200):
         print("Successfully sent e-mail! :)")
     else:
         print(r.status_code, r.reason)
 
-def mountString(best):
-    return("Flight " + best[1] + ": R$" + str(best[0]))
+def mountString(best, day, month):
+    return("%02d/%02d Flight " % (day, month) + best[1] + ": R$" + str(best[0]))
 
-def searchFromLatam():
-    latamAPI = "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?country=BR&language=PT&home=pt_br&origin=MCZ&destination=CWB&departure=2018-05-23&adult=1&cabin=Y"
+def searchFromLatam(day, month, showAll):
+    latamAPI = "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?country=BR&language=PT&home=pt_br&origin=MCZ&destination=CWB&departure=2018-%02d-%02d&adult=1&cabin=Y" % (month, day)
+    #print(latamAPI)
     latam = requests.get(latamAPI)
     latam = json.loads(latam.content.decode())
     best = [99999, 0]
     for flight in latam["data"]["flights"]:
-        print("Flight: ", flight["flightCode"], " R$", flight["cabins"][0]["displayPrice"], sep='')
+        if (showAll): print("Flight: ", flight["flightCode"], " R$", flight["cabins"][0]["displayPrice"], sep='')
         if (flight["cabins"][0]["displayPrice"] < best[0]):
             best = [flight["cabins"][0]["displayPrice"], flight["flightCode"]]
-    print("Best Flight Found:", best)
-    sendEmail("Latam %s" % mountString(best))
+    print("Best Flight Found (%02d/%02d):" % (day, month), best)
+    print()
+    return(best)
 
-searchFromLatam()
+def bot(daysRange, month, showAll, targetPrice, interval):
+    while (True):
+        bestSoFar = [99999, 0, 0, 0]
+        for i in daysRange:
+            now = searchFromLatam(i, month, showAll)
+            if (now[0] <= targetPrice):
+                print("YaY, better then targetPrice (%02d/%02d):" % (i, month), now)
+                sendEmail(mountString(now, i, month))
+            if (now[0] < bestSoFar[0]):
+                bestSoFar = [now[0], now[1], i, month]
+        print("Best so far: %s" % mountString([bestSoFar[0], bestSoFar[1]], bestSoFar[2], bestSoFar[3]))
+        time.sleep(interval)
 
+bot(range(21, 26 + 1), 5, 1, 400, 60 * 10)
