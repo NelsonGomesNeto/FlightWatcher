d765e81a209f211665ca6c4cf0851ebcac1e5cf6
NelsonGomesNeto <ngn@ic.ufal.br>
2018-08-20 15:56:37 -0300

    Implemented year support; still needs to user Date() though...

--- flightWatcher.py
+++ flightWatcher.py
@@ -14,11 +14,11 @@ def sendEmail(email, message):
     else:
         print(r.status_code, r.reason)
 
-def mountString(best, day, month):
-    return("%02d/%02d Flight " % (day, month) + best[1] + ": R$" + str(best[0]))
+def mountString(best, day, month, year):
+    return("%02d/%02d/%04d Flight " % (day, month, year) + best[1] + ": R$" + str(best[0]))
 
-def searchFromLatam(day, month, origin, destination, verbose):
-    latamAPI = "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?country=BR&language=PT&home=pt_br&origin=%s&destination=%s&departure=2018-%02d-%02d&adult=1&cabin=Y" % (origin, destination, month, day)
+def searchFromLatam(day, month, year, origin, destination, verbose):
+    latamAPI = "https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/revenue/recommendations/oneway?country=BR&language=PT&home=pt_br&origin=%s&destination=%s&departure=%04d-%02d-%02d&adult=1&cabin=Y" % (origin, destination, year, month, day)
     latam = requests.get(latamAPI)
     latam = json.loads(latam.content.decode())
     best = [99999, ""] # [price, flightCode]
@@ -26,29 +26,28 @@ def searchFromLatam(day, month, origin, destination, verbose):
         if (verbose): print("Flight: ", flight["flightCode"], " R$", flight["cabins"][0]["displayPrice"], sep='')
         if (flight["cabins"][0]["displayPrice"] < best[0]):
             best = [flight["cabins"][0]["displayPrice"], flight["flightCode"]]
-    print("Best Flight Found (%02d/%02d): R$%.2lf (Flight: %s)" % (day, month, best[0], best[1]))
-    print()
+    print("Best Flight Found (%02d/%02d/%04d): R$%.2lf (Flight: %s)\n" % (day, month, year, best[0], best[1]))
     return(best)
 
-def bot(daysRange, month, targetPrice, interval, origin, destination, emails, verbose):
+def bot(daysRange, month, year, targetPrice, interval, origin, destination, emails, verbose):
     bestSoFar = [99999, "", 0, 0, time.time()] # [price, flightCode, day, month, searchTime]
     while (True):
         for day in daysRange:
-            try: now = searchFromLatam(day, month, origin, destination, verbose)
+            try: now = searchFromLatam(day, month, year, origin, destination, verbose)
             except:
                 print("ERROR: Couldn't connect (Retrying in %ds)" % interval)
                 break
             if (now[0] <= targetPrice):
-                print("YaY, better then targetPrice (%02d/%02d):" % (day, month), now)
+                print("YaY, better then targetPrice (%02d/%02d/%04d):" % (day, month, year), now)
                 try:
                     for email in emails:
-                        sendEmail(email, mountString(now, day, month))
+                        sendEmail(email, mountString(now, day, month, year))
                 except:
                     print("ERROR: Couldn't send e-mail (Retrying in %ds)" % interval)
                     break
             if (now[0] < bestSoFar[0] or (now[0] == bestSoFar[0] and day < bestSoFar[2] and theEarlierTheBetter)):
-                bestSoFar = [now[0], now[1], day, month, time.time()]
-        print("Best so far: %s" % mountString([bestSoFar[0], bestSoFar[1]], bestSoFar[2], bestSoFar[3]), "| searching time: %lfs" % (time.time() - bestSoFar[4]))
+                bestSoFar = [now[0], now[1], day, month, year, time.time()]
+        print("Best so far: %s" % mountString([bestSoFar[0], bestSoFar[1]], bestSoFar[2], bestSoFar[3], bestSoFar[4]), "| searching time: %lfs" % (time.time() - bestSoFar[5]))
         time.sleep(interval)
 
 def readInput():
@@ -58,6 +57,8 @@ def readInput():
     lastDay = int(input())
     print("Month: ", end='')
     month = int(input())
+    print("Year: ", end='')
+    year = int(input())
     print("Threshold price: ", end='')
     thresholdPrice = int(input())
     print("Interval: ", end='')
@@ -70,10 +71,10 @@ def readInput():
     emails = input().split()
     print("Show each step (0, 1): ")
     verbose = int(input())
-    return(firstDay, lastDay, month, thresholdPrice, interval, origin, destination, emails, verbose)
+    return(firstDay, lastDay, month, year, thresholdPrice, interval, origin, destination, emails, verbose)
 
 arguments = sys.argv
 if (len(sys.argv) < 7):
     arguments = [0, 21, 26, 5, 1, 400, 600]
     arguments = [0] + list(readInput())
-bot(range(int(arguments[1]), int(arguments[2]) + 1), int(arguments[3]), int(arguments[4]), float(arguments[5]), arguments[6], arguments[7], arguments[8], int(arguments[9]))
+bot(range(int(arguments[1]), int(arguments[2]) + 1), int(arguments[3]), int(arguments[4]), float(arguments[5]), float(arguments[6]), arguments[7], arguments[8], arguments[9], int(arguments[10]))
